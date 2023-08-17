a795210ca9b90d02492b9d7e3d88348b73d06072
arthur oliveira <129237472+arthurzi03@users.noreply.github.com>
2023-08-17 11:01:23 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    float numero1;
+    float numero2;
+    float resultado;
+
+    numero1 = 41.10;
+    numero2 = 11.20;
+    resultado = numero1 * numero2;
+
+
+    printf ("resultado: %.2f\n", resultado);
+
+    return 0;
+}
+
